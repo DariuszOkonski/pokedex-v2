df9f6aaf2de7c1ba52f6d2c72c06f48ee7d5b54b
Dariusz Okonski <darek200180@gmail.com>
2021-02-18 21:40:26 +0000

    build Pokecard component

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## simple app in create react app
 
 ### API:
+### https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png
--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Pokecard from './Pokecard';
 
 function App() {
   return (
     <div className="App">
-      
+      <Pokecard 
+        id={4}
+        name="Charmander"
+        type="fire"
+        exp={62}
+      />
     </div>
   );
 }
